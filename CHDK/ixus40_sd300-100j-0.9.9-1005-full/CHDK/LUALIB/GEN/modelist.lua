15ac17001f93d1fb38ede3bbf0fea3c970a7aaaf
ben-oxley <ben.oxley@gmail.com>
2010-12-23 13:51:58 +0000

    Added CHDK software to git

---
+++ CHDK/ixus40_sd300-100j-0.9.9-1005-full/CHDK/LUALIB/GEN/modelist.lua
@@ -0,0 +1,69 @@
+--[[
+!!! THIS FILE IS GENERATED. DO NOT EDIT. !!!
+array of mode names indexed by their mode number from include/modelist.h
+you may want to use LUALIB/capmode.lua instead
+--]]
+return {
+  "AUTO",
+  "P",  -- Called "camera manual" on many cameras without a true manual mode, only allows +/- Ev adjustment
+  "TV",
+  "AV",
+  "M",  -- note, use only for true manual modes that allow direct control of Tv/Av
+  "PORTRAIT",
+  "NIGHT_SCENE",  -- "night scene" mode. Note, this can be a dail position, or under the scene menu (SCN_NIGHT_SCENE).
+  "LANDSCAPE",
+  "VIDEO_STD",
+  "VIDEO_SPEED",
+  "VIDEO_COMPACT",
+  "VIDEO_MY_COLORS",
+  "VIDEO_COLOR_ACCENT",
+  "VIDEO_COLOR_SWAP",
+  "STITCH",
+  "MY_COLORS",
+  "SCN_UNDERWATER",
+  "SCN_NIGHT_SNAPSHOT",
+  "LONG_SHUTTER",  -- "long shutter" mode on cameras without true manual mode. Allows manual shutter >= 1 second, uses manual shutter value propcase. Usually found under func menu in "manual" mode.
+  "SCN_LANDSCAPE",
+  "COLOR_SWAP",
+  "SCN_SNOW",
+  "SCN_BEACH",
+  "SCN_FIREWORK",
+  "SCN_COLOR_ACCENT",
+  "SCN_COLOR_SWAP",
+  "VIDEO_HIRES",
+  "SCN_AQUARIUM",
+  "COLOR_ACCENT",
+  "SCN_NIGHT_SCENE",
+  "SCN_ISO_3200",
+  "SCN_SPORT",
+  "SCN_KIDS_PETS",
+  "INDOOR",
+  "KIDS_PETS",
+  "NIGHT_SNAPSHOT",
+  "DIGITAL_MACRO",
+  "SCN_FOLIAGE",
+  "VIDEO_TIME_LAPSE",
+  "SCN_INDOOR",
+  "SCN_PORTRAIT",
+  "SUPER_MACRO",
+  "VIDEO_PORTRAIT",  -- tx1 junk
+  "VIDEO_NIGHT",  -- tx1 junk
+  "VIDEO_INDOOR",  -- tx1 junk
+  "VIDEO_FOLIAGE",  -- tx1 junk
+  "VIDEO_SNOW",  -- tx1 junk
+  "VIDEO_BEACH",  -- tx1 junk
+  "VIDEO_AQUARIUM",  -- tx1 junk
+  "VIDEO_SUPER_MACRO",  -- tx1 junk
+  "VIDEO_STITCH",  -- tx1 junk
+  "VIDEO_MANUAL",  -- tx1 junk
+  "SPORTS",
+  "QUICK",
+  "SCN_SUNSET",
+  "SCN_CREATIVE_EFFECT",  -- "creative light effect", only known on ixus950_sd850
+  "EASY",
+  "SCN_DIGITAL_MACRO",
+  "SCN_STITCH",
+  "SCN_LONG_SHUTTER",  -- only known on sx1, d10
+  "LOWLIGHT",  -- g11
+  "SCN_NOSTALGIC",  -- s90
+}
